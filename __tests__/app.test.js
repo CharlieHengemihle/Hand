f278d01063a62fd1d4125d4da7254c74789d03d8
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-10 13:23:22 -0700

    got a little carried away and did the first 2 test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,64 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('index dwarves', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET /dwarves should return a list of dwarves', async () => {
+    const resp = await request(app).get('/dwarves');
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "Gimli",
+          "purpose": "Lord of the Glittering Caves",
+          "source": "Middle Earth",
+          "url": "https://hero.fandom.com/wiki/Gimli",
+        },
+        Object {
+          "id": "2",
+          "name": "Balin",
+          "purpose": "Lord of Moria",
+          "source": "Middle Earth",
+          "url": "https://hero.fandom.com/wiki/Balin",
+        },
+        Object {
+          "id": "3",
+          "name": "Scout",
+          "purpose": "Place flares and reach high places",
+          "source": "Deep Rock Galactic",
+          "url": "https://deeprockgalactic.fandom.com/wiki/Dwarves",
+        },
+        Object {
+          "id": "4",
+          "name": "Driller",
+          "purpose": "Use titanium drills and explosives to clear the way",
+          "source": "Deep Rock Galactic",
+          "url": "https://deeprockgalactic.fandom.com/wiki/Dwarves",
+        },
+        Object {
+          "id": "5",
+          "name": "Yagrum Bagran",
+          "purpose": "Master Crafter",
+          "source": "Corprusarium Bowels",
+          "url": "https://elderscrolls.fandom.com/wiki/Yagrum_Bagarn_(Morrowind)",
+        },
+      ]
+    `);
+  });
+  it('gets dwarf with detail', async () => {
+    const resp = await request(app).get('/dwarves/1');
+    const gimli = {
+      id: '1',
+      name: 'Gimli',
+      purpose: 'Lord of the Glittering Caves',
+      source: 'Middle Earth',
+      url: 'https://hero.fandom.com/wiki/Gimli',
+    };
+    expect(resp.body).toEqual(gimli);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,91 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dwarves;
+
+CREATE TABLE dwarves (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  purpose VARCHAR NOT NULL,
+  source VARCHAR NOT NULL,
+  url VARCHAR NOT NULL
+);
+
+INSERT INTO dwarves (name, purpose, source, url)
+VALUES
+('Gimli', 'Lord of the Glittering Caves', 'Middle Earth', 'https://hero.fandom.com/wiki/Gimli'),
+('Balin', 'Lord of Moria', 'Middle Earth', 'https://hero.fandom.com/wiki/Balin'),
+('Scout', 'Place flares and reach high places', 'Deep Rock Galactic', 'https://deeprockgalactic.fandom.com/wiki/Dwarves'),
+('Driller', 'Use titanium drills and explosives to clear the way', 'Deep Rock Galactic', 'https://deeprockgalactic.fandom.com/wiki/Dwarves'),
+('Yagrum Bagran', 'Master Crafter', 'Corprusarium Bowels', 'https://elderscrolls.fandom.com/wiki/Yagrum_Bagarn_(Morrowind)');
+
+-- DROP TABLE IF EXISTS elves;
+
+-- CREATE TABLE elves (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   name VARCHAR NOT NULL,
+--   purpose VARCHAR NOT NULL,
+--   source VARCHAR NOT NULL,
+--   url VARCHAR NOT NULL
+-- );
+
+-- INSERT INTO elves (name, purpose, source, url)
+-- VALUES
+-- ('Legolas Greenleaf', 'Prince of the Woodland Realm', 'Middle Earth', 'https://lotr.fandom.com/wiki/Legolas'),
+-- ('Celebrimbor', 'Lord of the Gwaith-i-Mírdain', 'Middle Earth', 'https://lotr.fandom.com/wiki/Celebrimbor'),
+-- ('Ernest J. Keebler', 'Head Elf', 'The Hollow Tree Factory', 'https://en.wikipedia.org/wiki/Keebler_Company#Keebler_Elves'),
+-- ('Remallia Haventree', 'Lead Delegate of the Harpers', 'Waterdeep', 'https://forgottenrealms.fandom.com/wiki/Remallia_Haventree'),
+-- ('Kiaransalee', 'Lady of the Dead', 'Threnody', 'https://forgottenrealms.fandom.com/wiki/Kiaransalee');
+
+-- DROP TABLE IF EXISTS goblins;
+
+-- CREATE TABLE goblins (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   name VARCHAR NOT NULL,
+--   purpose VARCHAR NOT NULL,
+--   source VARCHAR NOT NULL,
+--   url VARCHAR NOT NULL
+-- );
+
+-- INSERT INTO goblins (name, purpose, source, url)
+-- VALUES
+-- ('Legolas Greenleaf', 'Prince of the Woodland Realm', 'Middle Earth', 'https://lotr.fandom.com/wiki/Legolas'),
+-- ('Celebrimbor', 'Lord of the Gwaith-i-Mírdain', 'Middle Earth', 'https://lotr.fandom.com/wiki/Celebrimbor'),
+-- ('Ernest J. Keebler', 'Head Elf', 'The Hollow Tree Factory', 'https://en.wikipedia.org/wiki/Keebler_Company#Keebler_Elves'),
+-- ('Remallia Haventree', 'Lead Delegate of the Harpers', 'Waterdeep', 'https://forgottenrealms.fandom.com/wiki/Remallia_Haventree'),
+-- ('Kiaransalee', 'Lady of the Dead', 'Threnody', 'https://forgottenrealms.fandom.com/wiki/Kiaransalee');
+
+-- DROP TABLE IF EXISTS humans;
+
+-- CREATE TABLE humans (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   name VARCHAR NOT NULL,
+--   purpose VARCHAR NOT NULL,
+--   source VARCHAR NOT NULL,
+--   url VARCHAR NOT NULL
+-- );
+
+-- INSERT INTO humans (name, purpose, source, url)
+-- VALUES
+-- ('Legolas Greenleaf', 'Prince of the Woodland Realm', 'Middle Earth', 'https://lotr.fandom.com/wiki/Legolas'),
+-- ('Celebrimbor', 'Lord of the Gwaith-i-Mírdain', 'Middle Earth', 'https://lotr.fandom.com/wiki/Celebrimbor'),
+-- ('Ernest J. Keebler', 'Head Elf', 'The Hollow Tree Factory', 'https://en.wikipedia.org/wiki/Keebler_Company#Keebler_Elves'),
+-- ('Remallia Haventree', 'Lead Delegate of the Harpers', 'Waterdeep', 'https://forgottenrealms.fandom.com/wiki/Remallia_Haventree'),
+-- ('Kiaransalee', 'Lady of the Dead', 'Threnody', 'https://forgottenrealms.fandom.com/wiki/Kiaransalee');
+
+-- DROP TABLE IF EXISTS monsters;
+
+-- CREATE TABLE monsters (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   name VARCHAR NOT NULL,
+--   purpose VARCHAR NOT NULL,
+--   source VARCHAR NOT NULL,
+--   url VARCHAR NOT NULL
+-- );
+
+-- INSERT INTO monsters (name, purpose, source, url)
+-- VALUES
+-- ('Legolas Greenleaf', 'Prince of the Woodland Realm', 'Middle Earth', 'https://lotr.fandom.com/wiki/Legolas'),
+-- ('Celebrimbor', 'Lord of the Gwaith-i-Mírdain', 'Middle Earth', 'https://lotr.fandom.com/wiki/Celebrimbor'),
+-- ('Ernest J. Keebler', 'Head Elf', 'The Hollow Tree Factory', 'https://en.wikipedia.org/wiki/Keebler_Company#Keebler_Elves'),
+-- ('Remallia Haventree', 'Lead Delegate of the Harpers', 'Waterdeep', 'https://forgottenrealms.fandom.com/wiki/Remallia_Haventree'),
+-- ('Kiaransalee', 'Lady of the Dead', 'Threnody', 'https://forgottenrealms.fandom.com/wiki/Kiaransalee');
