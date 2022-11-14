b9b24e6876ef4028157d7d563d21d1d592130d9f
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-14 13:09:56 -0700

    goblin test #2 details

--- __tests__/goblin.test.js
+++ __tests__/goblin.test.js
@@ -10,11 +10,55 @@ describe('index goblins', () => {
   it('#GET /goblins should return a list of goblins', async () => {
     const resp = await request(app).get('/goblins');
     expect(resp.body).toMatchInlineSnapshot(`
-      Object {
-        "message": "Not Found",
-        "status": 404,
-      }
+      Array [
+        Object {
+          "id": "1",
+          "name": "John Goblikon",
+          "purpose": "Nekrogoblikon hype man",
+          "source": "Los Angeles, California",
+          "url": "https://twitter.com/johngoblikon?lang=en",
+        },
+        Object {
+          "id": "2",
+          "name": "Monte Gazlow",
+          "purpose": "Trade Prince of the Bilgewater Cartel",
+          "source": "Booty Bay",
+          "url": "https://wowpedia.fandom.com/wiki/Monte_Gazlowe",
+        },
+        Object {
+          "id": "3",
+          "name": "Abrianna Mirimm",
+          "purpose": "Skysybil",
+          "source": "Rosohna",
+          "url": "https://criticalrole.fandom.com/wiki/Abrianna_Mirimm",
+        },
+        Object {
+          "id": "4",
+          "name": "Riz \\"The Ball\\" Gukgak",
+          "purpose": "Student",
+          "source": "Aguefort Adventuring Academy",
+          "url": "https://dimension20.fandom.com/wiki/Riz_Gukgak",
+        },
+        Object {
+          "id": "5",
+          "name": "Robin Redcap",
+          "purpose": "Familiar to William de Soules",
+          "source": "Scotland",
+          "url": "https://mythnerd.com/famous-goblins/",
+        },
+      ]
     `);
+  });
+  it('gets a goblin with details', async () => {
+    const resp = await request(app).get('/goblins/1');
+    const john = {
+      id: '1',
+      name: 'John Goblikon',
+      purpose: 'Nekrogoblikon hype man',
+      source: 'Los Angeles, California',
+      url: 'https://twitter.com/johngoblikon?lang=en',
+    };
+    expect(resp.body).toEqual(john);
   });
   afterAll(() => {
     pool.end();
