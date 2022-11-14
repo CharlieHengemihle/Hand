a2cf38c917c95bd8573cd1fddf8c11b36a9e5675
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-14 11:19:11 -0700

    The first test of elves has passed

--- __tests__/elf.test.js
+++ __tests__/elf.test.js
@@ -10,10 +10,43 @@ describe('index elves', () => {
   it('#GET /elves should return a list of elves', async () => {
     const resp = await request(app).get('/elves');
     expect(resp.body).toMatchInlineSnapshot(`
-      Object {
-        "message": "Not Found",
-        "status": 404,
-      }
+      Array [
+        Object {
+          "id": "1",
+          "name": "Legolas Greenleaf",
+          "purpose": "Prince of the Woodland Realm",
+          "source": "Middle Earth",
+          "url": "https://lotr.fandom.com/wiki/Legolas",
+        },
+        Object {
+          "id": "2",
+          "name": "Celebrimbor",
+          "purpose": "Lord of the Gwaith-i-Mírdain",
+          "source": "Middle Earth",
+          "url": "https://lotr.fandom.com/wiki/Celebrimbor",
+        },
+        Object {
+          "id": "3",
+          "name": "Ernest J. Keebler",
+          "purpose": "Head Elf",
+          "source": "The Hollow Tree Factory",
+          "url": "https://en.wikipedia.org/wiki/Keebler_Company#Keebler_Elves",
+        },
+        Object {
+          "id": "4",
+          "name": "Remallia Haventree",
+          "purpose": "Lead Delegate of the Harpers",
+          "source": "Waterdeep",
+          "url": "https://forgottenrealms.fandom.com/wiki/Remallia_Haventree",
+        },
+        Object {
+          "id": "5",
+          "name": "Kiaransalee",
+          "purpose": "Lady of the Dead",
+          "source": "Threnody",
+          "url": "https://forgottenrealms.fandom.com/wiki/Kiaransalee",
+        },
+      ]
     `);
   });
 });
